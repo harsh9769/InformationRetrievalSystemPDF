9b1589f5dac1bfc4f129a01390202c4a887c60e8
Harsh <dalviharsh976@gmail.com>
2024-08-24 19:19:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name ="Generative Ai Project",
+    version="0.0.1",
+    author="Harsh",
+    author_email="dalviharsh976@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+
+)
